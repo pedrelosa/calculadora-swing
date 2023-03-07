0f8f861d545c8ebd2210306e8bc8f1f9af6bf4f5
pedrelosa <pedrelosa@hotmail.com>
2023-03-06 23:15:46 -0300

    feat implementando os enums para os botões

--- src/br/com/pedrelosa/calculadora/modelo/Memoria.java
+++ src/br/com/pedrelosa/calculadora/modelo/Memoria.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Memoria {
+	public enum TipoComando{
+		ZERAR, NUMERO, DIVISAO, MULTIPLICACAO, SUBTRACAO, SOMA, IGUAL, VIRGULA
+	}
 	private static final Memoria instancia = new Memoria();
 	private final List<MemoriaObservador> observadores = new ArrayList<>();
 	private String textoAtual = "";
@@ -20,9 +23,43 @@ public class Memoria {
 	public String getTextoAtual(){
 		return this.textoAtual.isEmpty() ? "0": this.textoAtual;
 	}
-	public void procecarComando(String valor){
-		this.textoAtual += valor;
+	public void procecarComando(String texto){
+		TipoComando tipoComando = detectarTipoComando(texto);
+		System.out.println(tipoComando);
+		
+		this.textoAtual += texto;
 		
 		this.observadores.forEach(o -> o.valorAlterado(this.getTextoAtual()));
+	}
+	
+	private TipoComando detectarTipoComando(String texto) {
+		
+		if (this.textoAtual.isEmpty() && texto.equals("0")){
+			return null;
+		}
+		
+		try {
+			Integer.parseInt(texto);
+			return TipoComando.NUMERO;
+		}catch (NumberFormatException e){
+			
+			if (texto.equals("AC")){
+				return TipoComando.ZERAR;
+			}else if (texto.equals("÷")){
+				return TipoComando.DIVISAO;
+			}else if (texto.equals("*")){
+				return TipoComando.MULTIPLICACAO;
+			}else if (texto.equals("-")){
+				return TipoComando.SUBTRACAO;
+			}else if (texto.equals("+")){
+				return TipoComando.SOMA;
+			}else if (texto.equals("=")){
+				return TipoComando.IGUAL;
+			}else if (texto.equals(",")){
+				return TipoComando.VIRGULA;
+			}
+		}
+		
+		return null;
 	}
 }
