e36ecdc892ae242928112d3f758b8bf96a91192a
pedrelosa <pedrelosa@hotmail.com>
2023-03-13 15:40:01 -0300

    feat: adicionando a funcionalidade dos operadores aritiméticos

--- src/br/com/pedrelosa/calculadora/modelo/Memoria.java
+++ src/br/com/pedrelosa/calculadora/modelo/Memoria.java
@@ -4,32 +4,76 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Memoria {
-	public enum TipoComando{
+	public enum TipoComando {
 		ZERAR, NUMERO, DIVISAO, MULTIPLICACAO, SUBTRACAO, SOMA, IGUAL, VIRGULA
 	}
+	
 	private static final Memoria instancia = new Memoria();
 	private final List<MemoriaObservador> observadores = new ArrayList<>();
+	
+	private TipoComando ultimaOperacao = null;
+	private boolean substituir = false;
 	private String textoAtual = "";
-	private Memoria(){
+	private String textoBuffer = "";
+	
+	private Memoria() {
 	
 	}
 	
-	public static Memoria getInstancia(){
+	public static Memoria getInstancia() {
 		return instancia;
 	}
-	public void adicionarObservadores(MemoriaObservador observador){
+	
+	public void adicionarObservadores(MemoriaObservador observador) {
 		observadores.add(observador);
 	}
 	public String getTextoAtual(){
 		return this.textoAtual.isEmpty() ? "0": this.textoAtual;
 	}
-	public void procecarComando(String texto){
+	public void procecarComando(String texto) {
 		TipoComando tipoComando = detectarTipoComando(texto);
 		System.out.println(tipoComando);
 		
-		this.textoAtual += texto;
+		if (tipoComando == null) {
+			return;
+		} else if (tipoComando == TipoComando.ZERAR) {
+			this.textoAtual = "";
+			this.textoBuffer = "";
+			this.substituir = false;
+			this.ultimaOperacao = null;
+		} else if (tipoComando == TipoComando.NUMERO || tipoComando == TipoComando.VIRGULA) {
+			this.textoAtual = this.substituir ? texto : this.textoAtual + texto;
+			this.substituir = false;
+		}else {
+			this.substituir = true;
+			this.textoAtual = obterResultadoOperacao();
+			this.textoBuffer = this.textoAtual;
+			this.ultimaOperacao = tipoComando;
+		}
 		
 		this.observadores.forEach(o -> o.valorAlterado(this.getTextoAtual()));
+	}
+	
+	private String obterResultadoOperacao() {
+		if (this.ultimaOperacao == null) return this.textoAtual;
+		
+		double numeroBuffer = Double.parseDouble(this.textoBuffer.replace(',', '.'));
+		double numeroAtual = Double.parseDouble(this.textoAtual.replace(',', '.'));
+		
+		double resultado = 0;
+		
+		switch (this.ultimaOperacao){
+			case SOMA -> resultado = numeroBuffer + numeroAtual;
+			case SUBTRACAO -> resultado = numeroBuffer - numeroAtual;
+			case MULTIPLICACAO -> resultado = numeroBuffer * numeroAtual;
+			case DIVISAO -> resultado = numeroBuffer / numeroAtual;
+		}
+		
+		String resultadoString = Double.toString(resultado).replace('.', ',');
+		boolean isInteger = resultadoString.endsWith(",0");
+		System.out.println(isInteger);
+		
+		return isInteger ? resultadoString.replace(",0", "") : resultadoString;
 	}
 	
 	private TipoComando detectarTipoComando(String texto) {
@@ -63,7 +107,7 @@ public class Memoria {
 					return TipoComando.IGUAL;
 				}
 				case "," -> {
-					return TipoComando.VIRGULA;
+					if (!this.textoAtual.contains(","))	return TipoComando.VIRGULA;
 				}
 			}
 		}
